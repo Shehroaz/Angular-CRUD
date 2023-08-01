93153409992f4cae7eeff74b48f151ec0c4da873
Shehroaz <muhammadshehroaz@gmail.com>
2023-08-01 16:50:48 +0500

    Employee list component added.
    - Api call integratedm fetching data from backend

---
+++ Frontend/employee-frontend-angular/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../service/employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit{
+
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+    console.log("Employee list component");
+    this.getEmployeesList();
+  }
+
+  private getEmployeesList(){
+    this.employeeService.getEmployeeList().subscribe(response => {
+      this.employees = response;
+    })
+  }
+
+}
